b334faf3ac506d69a4de9fde3c57aae7afc4b624
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-09-01 04:57:14 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const wordReversed =reversestring(word);
+  return word === wordReversed
 }
 
+function reversestring(word) {
+  return word.split("").reverse().join("");
+}
 /* 
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+  Declared wordReversed to reverstring 
+  split the word and the joined to reverse
+  
 */
 
 // You can run `node index.js` to view these console logs
